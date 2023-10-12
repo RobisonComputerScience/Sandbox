9fd68f1d9d13c8884b0dd69bc70e724c28550a2a
RobisonCSSE <alexander.robisoncs@gmail.com>
2023-10-12 08:27:26 -0400

    Add print lines
    I learned how to use vars and if statements

--- SandboxPlayground.py
+++ SandboxPlayground.py
@@ -2,3 +2,8 @@
 # Date: 10.11.2023
 # Program:  Sandbox Playground
 
+print("Hello Alex Robison!\n")
+print("\nI am looking forward to the weekend!!")
+monkey = 15
+if monkey >= 15:
+    print("\nTomorrow is friday")
